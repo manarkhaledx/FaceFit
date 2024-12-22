460bd98da6b2e02eaa431e70fa2aed473b161a94
manar <manarkhaledx@outlook.com>
2024-12-22 22:26:55 +0200

    AppBottomNavigation() function added and modified to navigate from home to product page

--- app/src/main/java/com/example/facefit/HomePageActivity.kt
+++ app/src/main/java/com/example/facefit/HomePageActivity.kt
@@ -1,27 +1,54 @@
 package com.example.facefit
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,6 +56,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.facefit.ui.theme.Blue1
 import com.example.facefit.ui.theme.FaceFitTheme
+import com.example.facefit.ui.theme.Gray600
+import com.example.facefit.ui.theme.LavenderBlue
 
 // Entry Point Activity
 class HomePageActivity : ComponentActivity() {
@@ -42,66 +71,60 @@ class HomePageActivity : ComponentActivity() {
         }
     }
 }
-@Preview
-@Composable
-fun HomePagePreview(showBackground: Boolean = true) {
-    FaceFitTheme {
-        EyewearScreen()
-    }
-}
 
 @Composable
 fun EyewearScreen() {
     val products = getProducts()
     val categories = getCategories()
 
-    var searchText by remember { mutableStateOf("") }
-    var isActive by remember { mutableStateOf(false) }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .padding(16.dp)
-    ) {
-        Spacer(modifier = Modifier.height(24.dp))
-        SearchBarWithCameraButton()
-        Spacer(modifier = Modifier.height(24.dp))
-        FeaturedImagesSection()
-        Spacer(modifier = Modifier.height(24.dp))
-        CategorySection(title = "Categories", categories = categories)
-        Spacer(modifier = Modifier.height(24.dp))
-        ProductSection(title = "Best Seller", products = products)
-        Spacer(modifier = Modifier.height(24.dp))
-        ProductSection(title = "New Arrivals", products = products)
+    Scaffold(
+        bottomBar = { AppBottomNavigation() }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(paddingValues)
+                .padding(16.dp)
+        ) {
+            Spacer(modifier = Modifier.height(24.dp))
+            SearchBarWithCameraButton()
+            Spacer(modifier = Modifier.height(24.dp))
+            FeaturedImagesSection()
+            Spacer(modifier = Modifier.height(24.dp))
+            CategorySection(title = "Categories", categories = categories)
+            Spacer(modifier = Modifier.height(24.dp))
+            ProductSection(title = "Best Seller", products = products)
+            Spacer(modifier = Modifier.height(24.dp))
+            ProductSection(title = "New Arrivals", products = products)
+        }
     }
 }
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBarWithCameraButton() {
-    var searchText by remember { mutableStateOf("") } // Track user input
+    var searchText by remember { mutableStateOf("") }
 
     Row(
-        modifier = Modifier
-            .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically, // Align items vertically
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         // Search Bar
         TextField(
-            value = searchText, // Bind input value
-            onValueChange = { searchText = it }, // Update text state
+            value = searchText,
+            onValueChange = { searchText = it },
             placeholder = { Text("Search", fontSize = 14.sp, color = Color.Gray) },
             shape = RoundedCornerShape(20.dp),
             singleLine = true,
-            textStyle = LocalTextStyle.current.copy(fontSize = 16.sp), // Adjust text size
+            textStyle = LocalTextStyle.current.copy(fontSize = 16.sp),
             modifier = Modifier
-                .weight(1f) // Make the search bar take up available space
-                .shadow(8.dp, shape = RoundedCornerShape(20.dp)) // Add shadow
-                .height(56.dp), // Match the height of the button
+                .weight(1f)
+                .height(56.dp),
             trailingIcon = {
                 Icon(
-                    painter = painterResource(id = R.drawable.search_icon), // Replace with your icon
+                    painter = painterResource(id = R.drawable.search_icon),
                     contentDescription = "Search Icon",
                     tint = Color.Gray
                 )
@@ -113,11 +136,11 @@ fun SearchBarWithCameraButton() {
             )
         )
 
-        Spacer(modifier = Modifier.width(8.dp)) // Add spacing between search bar and button
-
+        Spacer(modifier = Modifier.width(8.dp))
         CameraButton()
     }
 }
+
 @Composable
 fun CameraButton() {
     Box(
@@ -136,14 +159,10 @@ fun CameraButton() {
     }
 }
 
-
-
 @Composable
 fun FeaturedImagesSection() {
     val images = listOf(R.drawable.img_notfound, R.drawable.img_notfound)
-    LazyRow(
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
+    LazyRow(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
         items(images) { image ->
             Card(
                 modifier = Modifier
@@ -165,25 +184,20 @@ fun FeaturedImagesSection() {
 
 @Composable
 fun CategorySection(title: String, categories: List<Pair<String, Int>>) {
-    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
-    val itemWidth = screenWidth / (categories.size.coerceAtMost(3) + 1)
-
     Column {
         Text(title, style = MaterialTheme.typography.titleLarge)
         Spacer(modifier = Modifier.height(8.dp))
         LazyRow(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
             items(categories) { (category, imageResId) ->
                 Column(
-                    modifier = Modifier.width(itemWidth),
+                    modifier = Modifier.width(100.dp),
                     horizontalAlignment = Alignment.CenterHorizontally
-
                 ) {
                     Image(
                         painter = painterResource(id = imageResId),
                         contentDescription = category,
                         modifier = Modifier
                             .size(70.dp)
-                            .shadow(4.dp, shape = CircleShape)
                             .clip(CircleShape),
                         contentScale = ContentScale.Crop
                     )
@@ -199,7 +213,6 @@ fun CategorySection(title: String, categories: List<Pair<String, Int>>) {
         }
     }
 }
-
 
 @Composable
 fun ProductSection(title: String, products: List<Product>) {
@@ -218,8 +231,7 @@ fun ProductCard(product: Product) {
 
     Box {
         Card(
-            modifier = Modifier
-                .width(160.dp),
+            modifier = Modifier.width(160.dp),
             elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
             colors = CardDefaults.cardColors(containerColor = Color.White)
         ) {
@@ -249,9 +261,7 @@ fun ProductCard(product: Product) {
 
         IconButton(
             onClick = { isFavorite = !isFavorite },
-            modifier = Modifier
-                .align(Alignment.TopEnd)
-                .padding(4.dp)
+            modifier = Modifier.align(Alignment.TopEnd)
         ) {
             Icon(
                 painter = painterResource(
@@ -264,7 +274,83 @@ fun ProductCard(product: Product) {
     }
 }
 
+@Composable
+fun AppBottomNavigation() {
+    var selectedItem by remember { mutableStateOf(0) }
+    val context = LocalContext.current
 
+    val items = listOf(
+        NavigationItem("Home", R.drawable.home),
+        NavigationItem("Favourites", R.drawable.heart),
+        NavigationItem("Products", R.drawable.glass_icon),
+        NavigationItem("Cart", R.drawable.cart),
+        NavigationItem("Profile", R.drawable.profile)
+    )
+
+    BottomNavigation(
+        backgroundColor = Color.White,
+        modifier = Modifier.height(80.dp) // Set fixed height
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            items.forEachIndexed { index, item ->
+                BottomNavigationItem(
+                    icon = {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center,
+                            modifier = Modifier.fillMaxHeight()
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .clip(RoundedCornerShape(12.dp))
+                                    .background(
+                                        if (selectedItem == index) LavenderBlue else Color.Transparent
+                                    )
+                                    .padding(vertical = 4.dp, horizontal = 12.dp)
+                            ) {
+                                Icon(
+                                    painter = painterResource(id = item.iconResId),
+                                    contentDescription = item.title,
+                                    tint = if (selectedItem == index) Blue1 else Gray600,
+                                    modifier = Modifier.size(24.dp)
+                                )
+                            }
+
+                            Text(
+                                text = item.title,
+                                fontSize = 10.sp,
+                                color = if (selectedItem == index) Blue1 else Gray600
+                            )
+                        }
+                    },
+                    selected = selectedItem == index,
+                    onClick = {
+                        selectedItem = index
+                        if (item.title == "Products") {
+                            context.startActivity(Intent(context, AllProductsActivity::class.java))
+                        }
+                    },
+                    modifier = Modifier.weight(1f)
+                )
+            }
+        }
+    }
+}
+
+
+
+
+
+// Data Models
+private data class NavigationItem(val title: String, val iconResId: Int)
+
+data class Product(val name: String, val price: String)
+
+// Mock Data
 fun getProducts() = listOf(
     Product("Browline Glasses", "EGP 120"),
     Product("Round Frame Glasses", "EGP 150"),
@@ -278,4 +364,10 @@ fun getCategories() = listOf(
     "Sun Glasses" to R.drawable.sun_glasses
 )
 
-data class Product(val name: String, val price: String)
+@Preview(showBackground = true)
+@Composable
+fun HomePagePreview() {
+    FaceFitTheme {
+        EyewearScreen()
+    }
+}
